c0ed93cd3efcb7412baa7a9254cc389623fe0626
Aniruddh Doki <doki.aniruddh@gmail.com>
2022-11-09 10:31:03 -0500

    Update main.py

--- back-end/main.py
+++ back-end/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from database.settings import Base, engine, get_db
+from models import LinkCreate
+from database.crud import get_all_links, create_db_link, delete_db_link:
 
 app = FastAPI()
 
 @app.get("/api/health")
 def read_root():
     return "Hello World!"
+
+origins = [
+  "http://localhost:3000",
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"]
+  allow_headers=["*"],
+)
